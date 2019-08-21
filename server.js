3f3b7d1075626abb01320a3be6c684176ee8bb7d
bolajiayodeji <sirbeejay1@gmail.com>
2019-08-21 12:33:24 +0100

    :zap:

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('this works!')
+})
+
 app.listen(3000, ()=> {
     console.log('app is running on port 3000')
 })
+
+/*
+ * 
+ * / --> res = this works
+ * /signin --> POST = success/fail
+ * /register --> POST = user
+ * /profile//:userId --> GET = user
+ * /image --> PUT = user
+ */
