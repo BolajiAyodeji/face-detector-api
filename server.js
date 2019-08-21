968c84847c2df83cd7414881108d584880797f88
bolajiayodeji <sirbeejay1@gmail.com>
2019-08-21 12:54:12 +0100

    setup user routes

--- server.js
+++ server.js
@@ -1,9 +1,54 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'bolaji',
+            email: 'bolaji@gmail.com',
+            password: 'palmser',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'david',
+            email: 'david@gmail.com',
+            password: 'davor',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res) => {
-    res.send('this works!')
+    res.send(database.users)
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('success')
+        } else {
+            res.status(400).json('error logging in')
+        }
+})
+
+app.post('/register', (req, res) => {
+    const {email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1])
 })
 
 app.listen(3000, ()=> {
@@ -11,7 +56,6 @@ app.listen(3000, ()=> {
 })
 
 /*
- * 
  * / --> res = this works
  * /signin --> POST = success/fail
  * /register --> POST = user
