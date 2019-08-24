8da44dab9cd807a67c239c9a7f88e4ce1b0c6a18
bolajiayodeji <sirbeejay1@gmail.com>
2019-08-24 05:48:51 +0100

    setup knex

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'bolaji',
+      password : '',
+      database : 'face-detector'
+    }
+  });
+
+console.log(db.select('*').from('users'));
 
 const app = express();
 
