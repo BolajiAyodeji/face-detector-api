245745f020b19b2b674b51a7d5bd57d5ff883a85
bolajiayodeji <sirbeejay1@gmail.com>
2019-08-26 05:18:14 +0100

    fix vuln

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+const dotenv = require('dotenv').config()
+
+const app = new Clarifai.App({
+    apiKey: `713b791fc082439eb45c8b4e7083e151`
+});
+
+const handleApi = (req,res) => {
+    app.models
+       .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+       .then(data => {
+           res.json(data);
+       })
+       .catch(err => res.status(400).json('unable to use API'))
+}
+
 const handleImage = (db) => (req, res) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (db) => (req, res) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApi
 }
