aa7b45fa810d5048a58790c7763269ebf032279a
bolajiayodeji <sirbeejay1@gmail.com>
2019-08-26 08:36:19 +0100

    fixed heroku :unicorn:

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: `${process.env.DB_HOST}`,
-        user: `${process.env.DB_USER}`,
-        password: `${process.env.DB_PASS}`,
-        database: `${process.env.DB_NAME}`
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
